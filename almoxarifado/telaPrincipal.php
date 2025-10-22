8626f06b9fdf60a5786d568fae843c5447fac39b
pinheg5 <gabrielle_pinheiro@estudante.sesisenai.org.br>
2025-10-22 17:13:21 -0300

    nova tela front

---
+++ almoxarifado/telaPrincipal.php
@@ -0,0 +1,25 @@
+<?php
+$host = 'localhost';
+$db   = 'almoxarifado_utilidades';
+$usuario = 'root';
+$senha = '';
+
+$conn = new mysqli($host, $usuario, $senha, $db);
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+// Busca os equipamentos e o nome do responsável
+$sql = "
+SELECT e.nome AS equipamento,
+       e.localizacao,
+       e.quantidade,
+       e.marca,
+       f.nome AS responsavel
+FROM equipamentos e
+LEFT JOIN funcionarios f ON e.responsavel_id = f.id
+";
+
+$result = $conn->query($sql);
+?>
