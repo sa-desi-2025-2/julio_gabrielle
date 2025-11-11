4622a9c5491fe9d23396a76876c558249a59eb1a
julio-senai <julio_paes-junior@estudante.sesisenai.org.br>
2025-11-11 16:25:14 -0300

    mensagem de erro ao entra

--- almoxarifado/php/verificar_login.php
+++ almoxarifado/php/verificar_login.php
@@ -9,30 +9,37 @@ $senha = '';
 $conn = new mysqli($host, $usuario, $senha, $db);
 
 if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+
+    header("Location: ../html/entrarNaConta.html?erro=3");
+    exit();
 }
 
 
-if (!isset($_POST['RE']) || empty($_POST['RE'])) {
-    echo "<h3> RE não informado.</h3>";
-    echo "<a href='../html/entrarNaConta.html'>Voltar</a>";
+if (!isset($_POST['RE']) || empty(trim($_POST['RE']))) {
+
+    header("Location: ../html/entrarNaConta.html?erro=2");
     exit();
 }
 
 $re = $conn->real_escape_string($_POST['RE']);
 
-$sql = "SELECT * FROM funcionarios WHERE RE = '$re' AND ativo=1";
-$resultado = $conn->query($sql);
+
+$sql = "SELECT * FROM funcionarios WHERE RE = ? AND ativo=1";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $re);
+$stmt->execute();
+$resultado = $stmt->get_result();
 
 if ($resultado->num_rows > 0) {
     $funcionario = $resultado->fetch_assoc();
-
 
     $_SESSION['id_funcionario'] = $funcionario['id_funcionario'];
     $_SESSION['nome'] = $funcionario['nome'];
     $_SESSION['adm'] = $funcionario['adm'];
     $_SESSION['RE'] = $funcionario['RE'];
 
+    $stmt->close();
+    $conn->close();
  
     if ($funcionario['adm'] == 1) {
         header("Location: ../php.front/telaeditor.php");
@@ -43,10 +50,10 @@ if ($resultado->num_rows > 0) {
     }
 
 } else {
-    echo "<h3> RE não encontrado. Acesso negado.</h3>";
-    
-    echo "<a href='../html/entrarNaConta.html'>Voltar</a>";
-}
 
-$conn->close();
+    $stmt->close();
+    $conn->close();
+    header("Location: ../html/entrarNaConta.html?erro=1");
+    exit();
+}
 ?>
